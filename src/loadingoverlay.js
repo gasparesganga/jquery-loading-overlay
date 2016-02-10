994244219b9a7a8ab78d5bb57b583108648c25b8
Ibrahim Islam <ibrahim.islam@live.com>
2016-02-10 14:19:56 +0600

    show loading symbol on top of everything
    Loading gif or font awesome icon gets behind any elements also placed there. Adding z-index takes it on top of everything.

--- src/loadingoverlay.js
+++ src/loadingoverlay.js
@@ -63,7 +63,8 @@ LoadingOverlay - A flexible loading overlay jQuery plugin
                     "display"           : "flex",
                     "flex-direction"    : "column",
                     "align-items"       : "center",
-                    "justify-content"   : "center"
+                    "justify-content"   : "center",
+                    "z-index" : "9999"
                 }
             });
             if (settings.image) overlay.css({
